a79b8c784e9b2d2b334830eaea47193f23318a69
Sourabh-Tag <sourabh@tagrem.com>
2014-11-24 09:18:55 +0530

    first commit

---
+++ ReadExcel/src/Addition.java
@@ -0,0 +1,23 @@
+
+public class Addition {
+	
+	public final int VALUE = 1;
+	
+	public static void main(String args[]){
+		
+	add();
+	}
+	
+	public static int add(){
+		int a=1,b=3,c=0;
+		c=a+b;
+		
+		System.out.println("Addition is "+c);
+		return c;
+	}
+	
+	public void variable(){
+		
+	}
+
+}
---
+++ ReadExcel/src/Demo.java
@@ -0,0 +1,19 @@
+
+public class Demo {
+
+	static{
+		add();
+	}
+	
+	/*public static void main(String args[]){
+		System.out.println("Musa");
+	}*/
+	
+	public static int add(){
+		int a=1,b=3,c=0;
+		c=a+b;
+		
+		System.out.println("Addition is "+c);
+		return c;
+	}
+}
